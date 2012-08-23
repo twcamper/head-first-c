39e43c412654701a0196e9a9031f7f9e02326d24
Tim <twcamper@thoughtworks.com>
2012-08-22 23:44:57 -0500

    p. 27: switch statement

--- src/cards.c
+++ src/cards.c
@@ -4,20 +4,24 @@
 int main()
 {
   char card_name[3];
+
   puts("Enter the card name: ");
   scanf("%2s", card_name);
+
   int val = 0;
-  if (card_name[0] == 'K') {
-    val = 10;
-  } else if (card_name[0] == 'Q') {
-    val = 10;
-  } else if (card_name[0] == 'J') {
-    val = 10;
-  } else if (card_name[0] == 'A') {
+  switch (card_name[0]) {
+  case 'A':
     val = 11;
-  } else {
+    break;
+  case 'J':
+  case 'Q':
+  case 'K':
+    val = 10;
+    break;
+  default:
     val = atoi(card_name);
   }
+
   // check if the value is 3 to 6
   if (val > 2 && val < 7)
     puts("Count has gone up.");
