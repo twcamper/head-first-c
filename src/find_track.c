51e6f95c1bd024382cc594f03d5b4bf39edd457d
Tim <twcamper@thoughtworks.com>
2012-08-26 20:58:25 -0500

    find_track using scanf instead of fgets which would require a chomp of the input newline

---
+++ src/find_track.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+char tracks[][80] = {
+    "I left my heart at Harvard Med School",
+    "Newark, Newark, a wonderful town",
+    "Dancing with a Dork",
+    "From here to maternity",
+    "The girl from Iwo Jima"
+};
+
+void find_track(char search_for[])
+{
+  for (int i = 0; i < 5; i++)  {  // c99 allows inline int declaration
+    if (strstr(tracks[i], search_for))
+      printf("Track %i: '%s'\n", i, tracks[i]);
+  }
+}
+
+int main()
+{
+  char search_for[80];
+  int foont;
+  printf("Search for: ");
+  /*fgets(search_for, 80, stdin); // returns \n so the strstr() search fails*/
+  scanf("%79s", search_for);
+  find_track(search_for);
+  return 0;
+}
