b31e168f1c02382488dae1ab5da5b36c70310540
Tim <twcamper@thoughtworks.com>
2012-08-30 11:36:31 -0500

    one less global

--- src/totaller.c
+++ src/totaller.c
@@ -2,10 +2,10 @@
 
 float total = 0.0;
 short count =  0;
-short tax_percent = 6;
 
 float add_with_tax(float f)
 {
+  short tax_percent = 6;
   float tax_rate = 1 + tax_percent / 100.0;
   total += f * tax_rate;
   count++;
@@ -21,7 +21,8 @@ int main()
     printf("subtotal: %.2f\n", add_with_tax(val));
     printf("Price of item: ");
   }
-    printf("\nFinal total: %.2f\n", total);
-    printf("Number of items: %hi\n", count);
+
+  printf("\nFinal total: %.2f\n", total);
+  printf("Number of items: %hi\n", count);
   return 0;
 }
