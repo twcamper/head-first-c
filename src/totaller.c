0d42e0fd2c1f845e0936afb6843c882b2950e4d1
Tim <twcamper@thoughtworks.com>
2012-08-30 11:34:25 -0500

    numeric data types example

---
+++ src/totaller.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+float total = 0.0;
+short count =  0;
+short tax_percent = 6;
+
+float add_with_tax(float f)
+{
+  float tax_rate = 1 + tax_percent / 100.0;
+  total += f * tax_rate;
+  count++;
+
+  return total;
+}
+
+int main()
+{
+  float val;
+  printf("Price of item: ");
+  while (scanf("%f", &val) == 1) {
+    printf("subtotal: %.2f\n", add_with_tax(val));
+    printf("Price of item: ");
+  }
+    printf("\nFinal total: %.2f\n", total);
+    printf("Number of items: %hi\n", count);
+  return 0;
+}
