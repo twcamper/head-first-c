6a0c66b0e24b6e73327a598116dbb3f2c16d6b8c
Tim <twcamper@thoughtworks.com>
2012-09-10 20:59:37 -0500

    fork a process for each feed, pid_t not defined???

--- 09/newshound.c
+++ 09/newshound.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <errno.h>
+
+/*gcc cannot find pid_t for some reason on linux*/
+typedef __pid_t pid_t;
+
+void error(char *msg)
+{
+  fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+  exit(1);
+}
 
 int main(int argc, char *argv[])
 {
   const char *PYTHON = "/usr/bin/python";
   const char *SCRIPT = "09/rssgossip.py";
-  char *feeds[] = {"http://www.nytimes.com/services/xml/rss/nyt/Africa.xml"};
-  int   feed_count = 1;
+  char *feeds[] = {
+    "http://www.nytimes.com/services/xml/rss/nyt/Africa.xml",
+    "http://www.nytimes.com/services/xml/rss/nyt/Americas.xml",
+    "http://www.nytimes.com/services/xml/rss/nyt/MiddleEast.xml",
+    "http://www.nytimes.com/services/xml/rss/nyt/Europe.xml",
+    "http://www.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml"
+  };
+  int   feed_count = 5;
 
   char *search_phrase = argv[1];
 
@@ -16,10 +32,17 @@ int main(int argc, char *argv[])
     char var[255];
     sprintf(var, "RSS_FEED=%s", feeds[i]);
     char *vars[] = {var, NULL};
-    if (execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars) == -1) {
-      fprintf(stderr, "Can't run script: %s\n", strerror(errno));
-      return 1;
-    }
+
+    pid_t pid = fork();
+    if (pid == -1)
+      error("Can't fork process");
+
+    if (pid == 0) {
+      if (execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars) == -1) {
+        error("Can't run script");
+      }
+    } else
+      printf("forked process %i: %s\n", pid, feeds[i]);
   }
   return 0;
 }
