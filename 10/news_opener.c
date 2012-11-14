ecfff48a93aa953a2ccecde120ab5d47df8e85af
tim <twcamper@thoughtworks.com>
2012-11-14 04:07:00 -0600

    open the list of urls in the browser

--- 10/news_opener.c
+++ 10/news_opener.c
@@ -45,6 +45,53 @@ void open_urls(int url_count, char *urls[])
 int main(int argc, char *argv[])
 {
 
-  open_urls(argc - 1, argv + 1);
+  const char *PYTHON = "/usr/bin/python";
+  const char *SCRIPT = "09/rssgossip.py";
+  char *phrase = argv[1];
+  char *vars[] = {"RSS_FEED=http://www.nytimes.com/services/xml/rss/nyt/Americas.xml", NULL};
+
+  /*Create a pipe */
+  int fd[2];
+  if (pipe(fd) == -1)
+    error("Can't create the pipe");
+
+  /*Fork a child process*/
+  pid_t pid = fork();
+  if (pid == -1)
+    error("Can't fork process");
+
+  /*Child will be write-end of the pipe: will generate output by running python*/
+  if (!pid) {
+    /*stdout becomes write-end of pipe*/
+    dup2(fd[1],1);
+    /*closing read-end because we don't read over here*/
+    close(fd[0]);
+
+    if (execle(PYTHON, PYTHON, SCRIPT, "-u", phrase, NULL, vars) == -1)
+      error("Can't run script");
+  }
+
+  /* Parent process will be read-end */
+
+  /*stdin becomes read-end of pipe */
+  dup2(fd[0], 0);
+  /*closing write-end because all we do is read over here */
+  close(fd[1]);
+
+  /*will collect the urls from the python script at the other end of the pipe*/
+  char *urls[255];
+  char line[255];
+  int url_count = 0;
+
+  while (fgets(line,255,stdin)) {
+    if (line[0] == '\t')  {
+      urls[url_count] = strdup(&line[1]);
+      url_count++;
+    }
+  }
+
+  if (url_count > 0)
+    open_urls(url_count, urls);
+
   return 0;
 }
