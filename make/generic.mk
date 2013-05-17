9e18b0f60315af104af627c702a3bb91694e2213
Tim Camper <twcamper@thoughtworks.com>
2013-05-17 07:57:25 -0500

    remove useless requirement to name 'all' target

--- make/generic.mk
+++ make/generic.mk
@@ -7,9 +7,6 @@ EXECUTABLES = $(SRCS:.c=)
 H_SRCS      = $(shell find . -name '*.h' | tr '\n' ' ')
 HEADERS     = $(H_SRCS:.h=.gch)
 
-warn:
-	@echo You probably mean "'all'"
-
 #### Build all executable targets, using a 'Static Pattern Rule' (GNU make manual, 4.11) ####
 $(EXECUTABLES) : % : %.o
 	$(LD) $< -o $@
